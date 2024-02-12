9aa61f05548bc8f6a9e9267f7ada847871f318d7
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-02-12 22:25:36 +0600

    new setup for sort drag and drop

--- js/main.js
+++ js/main.js
@@ -6,31 +6,3 @@ const list = document.querySelectorAll(".iist");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const arrList = Array.from(list);
-
-let dragitem = null;
-arrList.map((listItem) => {
-  listItem.addEventListener("dragstart", (e) => {
-    dragitem = e.target;
-  });
-
-  right.addEventListener("dragover", (event) => {
-    event.preventDefault();
-  });
-
-  right.addEventListener("drop", (event) => {
-    event.preventDefault();
-
-    right.appendChild(dragitem);
-    dragitem = null;
-  });
-
-  left.addEventListener("dragover", (event) => {
-    event.preventDefault();
-  });
-
-  left.addEventListener("drop", (event) => {
-    event.preventDefault();
-    left.appendChild(dragitem);
-    dragitem = null;
-  });
-});
