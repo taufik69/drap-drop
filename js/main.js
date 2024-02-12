288c3a69720e4f4353f97b07267e42b35dea0df4
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-02-12 23:33:55 +0600

    stuck in reduce method

--- js/main.js
+++ js/main.js
@@ -2,7 +2,36 @@
  * take all list className
  */
 
-const list = document.querySelectorAll(".iist");
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const arrList = Array.from(list);
+const draggable = document.querySelectorAll(".draggable");
+const container = document.querySelectorAll(".container");
+draggable.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
+
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  });
+});
+
+container.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    /**
+     *  when drag any dragable component then dynamically add dragging class then
+     * select the dragging classname and putting this element or tag into container div
+     * whereas no need to drop eventlisher
+     */
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    container.appendChild(draggable);
+  });
+});
+
+function getDragAfterElement(container, y) {
+  const draggeableElement = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
+  console.clear();
+  console.log(draggeableElement);
+}
